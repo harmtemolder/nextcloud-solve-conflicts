18170a957ac1cc7f75065b97c7174a446e7c89ea
Harm te Molder <mail@harmtemolder.com>
2021-03-29 20:44:46 -0300

    Ask input for what to do with conflicts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# nextcloud_solve_conflicts
-This script parses the local nextcloud folder solving the conflicts by retaining the most recently modified file.
+# Solve Nextcloud conflicts
+
+This script searches the local [Nextcloud](https://nextcloud.com/) folder for sync conflicts and asks how to resolve each of these. It assumes that the local file is the one renamed to `(conflicted copy)` and the server file is the one with the original filename.
+
+## Credits
+
+- Uses [inquirer](https://python-inquirer.readthedocs.io/en/latest/) (`conda install inquirer`) for the prompts;
+- [pathlib](https://docs.python.org/3/library/pathlib.html) for file path operations;
+- and [datetime](https://docs.python.org/3/library/datetime.html) to display file modification times in a readable format
---
+++ nextcloud-solve-conflicts.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from pathlib import Path, PurePath
+
+import inquirer
+
+def ensure(path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    return path
+
+rootdir = Path('~/Nimbus/Books').expanduser()
+trashdir = rootdir / '.Trash'
+
+conflicts = rootdir.glob('**/* (conflicted copy *')
+
+for conflict in conflicts:
+    original = (conflict.parent / Path(
+        conflict.stem[:conflict.stem.find(" (conflicted copy ")]
+        + conflict.suffix))
+
+    if not original.exists():
+        conflict.rename(original)
+        continue
+
+    conflict_relative = PurePath(conflict).relative_to(rootdir)
+    original_relative = PurePath(original).relative_to(rootdir)
+
+    conflict_mtime = datetime.fromtimestamp(conflict.stat().st_mtime)
+    original_mtime = datetime.fromtimestamp(original.stat().st_mtime)
+
+    conflict_choice = 'Local  ({}): {}'.format(
+        conflict_mtime.isoformat(timespec='seconds'),
+        conflict_relative,
+    )
+    original_choice = 'Server ({}): {}'.format(
+        original_mtime.isoformat(timespec='seconds'),
+        original_relative,
+    )
+
+    questions = [inquirer.List(
+        'keep',
+        message='Which file(s) do you want to keep?',
+        choices=[conflict_choice, original_choice, 'both', 'quit']
+    )]
+
+    answers = inquirer.prompt(questions)
+
+    if answers['keep'] == conflict_choice:
+        original.rename(ensure(trashdir / original_relative))
+        conflict.rename(ensure(original))
+        print('Kept local file, moved server file to {}'.format(trashdir))
+    elif answers['keep'] == original_choice:
+        conflict.rename(ensure(trashdir / conflict_relative.parent / original.name))
+        print('Kept server file, moved local file to {}'.format(trashdir))
+    elif answers['keep'] == 'quit':
+        break
